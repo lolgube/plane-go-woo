6fc4f1f0d6bb659046048ec905eebf7b0378511a
Mohammed Osman <mohammed.osman@elev.ga.lbs.se>
2021-05-02 16:23:54 +0200

    Initial commit

---
+++ Assets/Scripts/BG_Scroll.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// -- made by mohammed
+
+public class BG_Scroll : MonoBehaviour
+{
+    
+    public float scrollingSpeed = 4f;
+    private Vector3 StartPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // this moves our background downwards. i.e moving down times deltatime times our scroll variable. 
+        transform.Translate(Vector3.down*scrollingSpeed*Time.deltaTime);
+
+        // once the image moves past this y value, move it back to the top of the screen.
+        if(transform.position.y < -29.11997){
+            transform.position = StartPos;
+        }
+    }
+}
---
+++ Assets/Scripts/SpaceShip.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceShip : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float speed;
+
+    // gets our rb
+    private void Awake() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Start() {
+        
+    }
+
+    //movement times speed
+    void Update() {
+        rb.AddForce(new Vector2(Input.GetAxis("Horizontal")* speed,0));
+        rb.AddForce(new Vector2(0,Input.GetAxis("Vertical")* speed));
+    }
+}
