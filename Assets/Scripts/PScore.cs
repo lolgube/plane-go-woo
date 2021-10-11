d6654b35240f4f5e879d4e38db40ceb3f2017790
lolgube010 <lolgube@gmail.com>
2021-10-11 14:47:44 +0200

    SMALLSTUFF1

---
+++ Assets/Scripts/PScore.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PScore : MonoBehaviour
+{
+    AudioManager aM;
+    private void Start()
+    {
+        aM = FindObjectOfType<AudioManager>();
+    }
+    // if collide, give player pscore and then go away
+    private void OnTriggerEnter2D(Collider2D col) {
+        if(col.gameObject.tag == "Player"){
+            //////////////////////////////////////////////////
+            // CHANGE THIS SOUND FOR P-SCORE AT SOME POINT ///
+            //////////////////////////////////////////////////
+            FindObjectOfType<AudioManager>().Play("Health pickup");
+            SpaceShip.health++;
+            Destroy(gameObject);
+        }
+    }
+}
