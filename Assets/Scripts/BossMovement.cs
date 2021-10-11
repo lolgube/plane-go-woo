0b45087333ca837a7ab364cd2702b609d81e2f4d
ElioMio <elio.enestrom@elev.ga.lbs.se>
2021-10-11 14:21:27 +0200

    Movement
    Made changes in BossMovement

--- Assets/Scripts/BossMovement.cs
+++ Assets/Scripts/BossMovement.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class BossMovement : MonoBehaviour
 {
+    public Rigidbody bossrb;
+    public GameObject bossLady;
     // Start is called before the first frame update
     void Start()
     {
