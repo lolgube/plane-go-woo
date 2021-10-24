67d8138eb1b3057bf8092b9b3bbf777db37cd315
Mohammed Osman <mohammed.osman@elev.ga.lbs.se>
2021-10-24 20:31:41 +0200

    a bunch of fixes, no real progress on p score yet. sorry
    please put the dummy gameobject into whatever slot you need filled if you're using enemy.cs

--- Assets/Scripts/Spawn.cs
+++ Assets/Scripts/Spawn.cs
@@ -18,7 +18,9 @@ public class Spawn : MonoBehaviour {
     // spawns a random enemy from the array, from the left to the right side of the screen on y level 7, 
     // making sure they're facing the right direction
     // instantiate works by gameobject - position - direction/rotation
-    // fthe for loop decides how many times we're running this code, the higher the waves in the more things it spawns
+    // the for loop decides how many times we're running this code, the higher the waves in the more things it spawns
+    
+    // new edit; maybe change this line so that we can specify how often we want a specific enemy to spawn, and not just a random one. like maybe make another .cs just for asteroids maybe?
     void SpawnEnemy() {  
         for(int i=0; i<waves;i++)
             Instantiate(enemies[(int)Random.Range(0,enemies.Length)], new Vector3(Random.Range(-8f,8f),7,10), Quaternion.identity);
