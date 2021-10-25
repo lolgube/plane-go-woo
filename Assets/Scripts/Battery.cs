890cf4ef48e8f44e933a4e3146b370bf85ebe416
lolgube010 <lolgube@gmail.com>
2021-10-25 14:00:58 +0200

    might've broken something in the game scene, sorry if i did

--- Assets/Scripts/Battery.cs
+++ Assets/Scripts/Battery.cs
@@ -5,10 +5,20 @@ using UnityEngine;
 public class Battery : MonoBehaviour
 {
     AudioManager aM;
-    private void Start()
-    {
+    Rigidbody2D rb;
+    // sets our y speed
+    public float ySpeed;
+
+
+    private void Start() {
         aM = FindObjectOfType<AudioManager>();
+        rb = GetComponent<Rigidbody2D>();
     }
+    // makes the thing fall slowly
+    private void Update() {
+        rb.velocity = new Vector2(0,ySpeed*-1);
+    }
+
     // if collide, give player health and then go away
     private void OnTriggerEnter2D(Collider2D col) {
         if(col.gameObject.tag == "Player"){
--- Assets/Scripts/PScore.cs
+++ Assets/Scripts/PScore.cs
@@ -4,26 +4,29 @@ using UnityEngine;
 
 
 // all this script does is make sure the prefab functionality works fine
-
 public class PScore : MonoBehaviour
 {
     AudioManager aM;
+    Rigidbody2D rb;
     [SerializeField]
     bool isThisAMaxItem;
     int pscoreWorth;
     // p-scoreMaxOut puts your p-score at the highest, could be a useful drop.
     int pScoreMaxOut = 100;
+    // sets our y speed
+    public float ySpeed;
 
     private void Start() {
         aM = FindObjectOfType<AudioManager>();
         pscoreWorth = Mathf.Clamp(pscoreWorth, 0, 100);
+        rb = GetComponent<Rigidbody2D>();
     }
     // if collide, give player pscore and then go away
     private void OnTriggerEnter2D(Collider2D col) {
         if(col.gameObject.tag == "Player"){
-            //////////////////////////////////////////////////
-            // CHANGE THIS SOUND FOR P-SCORE AT SOME POINT ///
-            //////////////////////////////////////////////////
+            /////////////////////////////////////////////////
+            // CHANGE THIS SOUND FOR P-SCORE AT SOME POINT //
+            /////////////////////////////////////////////////
             FindObjectOfType<AudioManager>().Play("Health pickup");
 
             // randomizes small p-score thingie worth
@@ -45,8 +48,10 @@ public class PScore : MonoBehaviour
             
         }
     }
-
     private void pScoreRandomizeWorth() {
         pscoreWorth = Random.Range(1, 5);
+    }
+    private void Update() {
+        rb.velocity = new Vector2(0,ySpeed*-1);
     }
 }
